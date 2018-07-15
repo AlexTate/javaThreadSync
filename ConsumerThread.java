fa683018d2837102933fb4176a2df33b44c3b9c7
AlexTate <0xalextate@gmail.com>
2018-07-15 16:55:22 -0600

    Update ConsumerThread.java

--- ConsumerThread.java
+++ ConsumerThread.java
@@ -7,10 +7,10 @@ import java.text.SimpleDateFormat;
 public class ConsumerThread extends Thread {
 	
 	private static Random rand = new Random();			//For generating random wait times between consumes
-	public static BoundedBuffer buff;					//The bounded buffer (circular FIFIO)
-	public static int bufferSize;						//Size of buffer (surprise)
-	public static int maxWait;							//Maximum time to wait between consumption attempts
-	private int waitCount = 0;							//For tracking 10x max wait limitation
+	public static BoundedBuffer buff;				//The bounded buffer (circular FIFIO)
+	public static int bufferSize;					//Size of buffer (surprise)
+	public static int maxWait;					//Maximum time to wait between consumption attempts
+	private int waitCount = 0;					//For tracking 10x max wait limitation
 	private int id;
 	
 	
@@ -36,16 +36,16 @@ public class ConsumerThread extends Thread {
 	public void run() {		
 		try {			
 		
-		while (waitCount < 10) {						//Loop until 10x maxWait
+		while (waitCount < 10) {					//Loop until 10x maxWait
 			synchronized(buff) {
 				
 				int[] result = buff.fetch();			//result[0] = position, result[1] = fetched value				
-				if (result[0] == -1) {					//The buffer was empty
+				if (result[0] == -1) {				//The buffer was empty
 					
 					//Prints only once per successive failure
 					if (waitCount == 0) System.out.printf("Consumer %2d: Unable to consume, buffer empty, at Time: %s\n", id, timeStamp());
 					
-					buff.wait(genWait());				//Wait [1,maxWait] ms
+					buff.wait(genWait());			//Wait [1,maxWait] ms
 					waitCount++;
 									
 				} else {
@@ -53,21 +53,21 @@ public class ConsumerThread extends Thread {
 					System.out.printf("Consumer %2d: Removed %2d from Location: %2d at Time: %s\n", 
 										id, result[1], result[0], timeStamp());	
 					
-					buff.notifyAll();					//Wake all other waiting threads now that we're out of critical section
-					waitCount = 0;						//Fetch was successful, reset the 10-wait counter
-					//buff.print();						//Print contents of buffer, nicely (useful for debugging)
+					buff.notifyAll();			//Wake all other waiting threads now that we're out of critical section
+					waitCount = 0;				//Fetch was successful, reset the 10-wait counter
+					//buff.print();				//Print contents of buffer, nicely (useful for debugging)
 					
 				}
 			}
 			
-			Thread.sleep(genWait());					//Wait a moment to simulate uncertainty in timing
+			Thread.sleep(genWait());				//Wait a moment to simulate uncertainty in timing
 		}
 		
 		System.out.printf("Consumer %2d: terminated after 10 times its maximum waiting time at Time: %s\n", id, timeStamp());
 		return;
 			
 		} catch (InterruptedException ie) {
-            System.out.printf("\tInterrupted! Consumer ID reporting: %d", this.id);
+            		System.out.printf("\tInterrupted! Consumer ID reporting: %d", this.id);
 		}
 	}
 	
